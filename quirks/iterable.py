6b2d6a452f8d479a32772c49309153456413f8da
Peter Boráros <pborky@pborky.sk>
2013-07-14 01:48:10 +0200

    initial

---
+++ quirks/iterable.py
@@ -0,0 +1,6 @@
+
+from itertools import *
+
+def isiterable(obj):
+    from collections import Iterable
+    return isinstance(obj, Iterable)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Python Quirks',
+      version='0.1',
+      description='Small and handy python routines',
+      author='Peter Boraros',
+      author_email='pborky@pborky.sk',
+      url='https://github.com/pborky/python-quirks',
+      packages=['quirks',],
+     )
