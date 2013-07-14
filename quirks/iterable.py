1707f0439a9b07b8d68e9b209d9dbaf417e5c134
Peter Boráros <pborky@pborky.sk>
2013-07-14 03:53:26 +0200

    take first n elements

--- quirks/iterable.py
+++ quirks/iterable.py
@@ -4,3 +4,8 @@ from itertools import *
 def isiterable(obj):
     from collections import Iterable
     return isinstance(obj, Iterable)
+
+
+def itake(n, it):
+    from itertools import izip,imap,count,takewhile
+    return imap(lambda x:x[1],takewhile(lambda x:x[0]<n, izip(count(),it)))    
